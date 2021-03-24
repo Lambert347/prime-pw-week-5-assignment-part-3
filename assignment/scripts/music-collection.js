cb3b855d1597ac59be8e510b45b3831b1ecac57f
Ian Lambert <lambe347@umn.edu>
2021-03-24 10:48:29 -0500

    Finally got showCollection to work

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -1,1 +1,33 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+  let album = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+  }
+  collection.push(album);
+  return album;
+}
+
+console.log(addToCollection('Heroes', 'Sabaton', '2014'));
+console.log(addToCollection('Appeal to Reason', 'Rise Against', '2008'));
+console.log(addToCollection('Star Eater', 'Daniel Deluxe', '2015'));
+console.log(addToCollection('A Place Behind the Moon', 'Big Gigantic', '2010'));
+console.log(addToCollection('20/20 Vision', 'Antiflag', '2020'));
+console.log(addToCollection('History Books', 'Absofacto', '2015'));
+
+console.log(collection);
+console.log(collection[2]);
+
+
+function showCollection(array) {
+  console.log(array.length);
+  for (let i = 0; i < array.length; i++) {
+    console.log(collection[i].title + ' by ' + collection[i].artist + ' published in ' + collection[i].yearPublished);
+  }
+}
+
+showCollection(collection);
