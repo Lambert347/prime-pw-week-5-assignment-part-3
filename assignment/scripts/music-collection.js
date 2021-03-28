eb8c2cdf0c874645db69a3f1aed3f8ac0bb7be50
Ian Lambert <lambe347@umn.edu>
2021-03-28 18:09:15 -0400

    Finished adding comments to the program

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -2,14 +2,28 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 
+//Initializes empty arrays to correspond to the tracks array for each album.
 let tracksGreat = [];
 let tracksReason = [];
 let tracksEater = [];
 let tracksNight = [];
 let tracksConnector = [];
 let tracksBooks = [];
-console.log(tracksGreat.length);
 
+//Function that adds track names and durations to a tracks array. The function takes the associated tracks array for the track as input, then the track name, and finally duration.
+//Then the function fills out a track object with the name and duration, then pushes that track to the tracks array indicated in the input. Then it returns the track.
+function addToTracks(tracks, name, duration) {
+  let track = {
+    name: name,
+    duration: duration
+  }
+  tracks.push(track);
+  return track;
+}
+
+
+//The following monstrosity is running the addToTracks function and adding the various track names and durations for Sabaton's The Great War album.
+//At the end of the code block, the console.log confirms that every track was added.
 addToTracks(tracksGreat, 'The Future of Warfare', '3:26');
 addToTracks(tracksGreat, 'Seven Pillars of Wisdom', '3:02');
 addToTracks(tracksGreat, '82nd All the Way', '3:31');
@@ -23,6 +37,7 @@ addToTracks(tracksGreat, 'The End of the War to End All Wars', '4:45');
 addToTracks(tracksGreat, 'In Flanders Fields', '1:57');
 console.log(tracksGreat);
 
+//Similar to above but with the tracks for Rise Against's Appeal to Reason album.
 addToTracks(tracksReason, 'Collapse (Post-Amerika)', '3:19');
 addToTracks(tracksReason, 'Long Forgotten Sons', '4:02');
 addToTracks(tracksReason, 'Re-Education (Through Labor)', '3:42');
@@ -39,9 +54,11 @@ addToTracks(tracksReason, 'Whereabouts Unknown', '4:02');
 addToTracks(tracksReason, 'Elective Amnesia', '3:55');
 console.log(tracksReason);
 
+//Similar to above but with Daniel Deluxe's Star Eater single.
 addToTracks(tracksEater, 'Star Eater', '4:33');
 console.log(tracksEater);
 
+//Adding the tracks and durations for Big Gigantic's the Night is Young album.
 addToTracks(tracksNight, 'Clvdbvrst', '4:39');
 addToTracks(tracksNight, 'Blue Dream', '4:16');
 addToTracks(tracksNight, 'The Night is Young (feat. Cherub)', '4:25');
@@ -52,6 +69,7 @@ addToTracks(tracksNight, 'Just for the Thrill', '4:29');
 addToTracks(tracksNight, 'Shooting Stars', '3:46');
 console.log(tracksNight);
 
+//Adding the tracks and durations for I the Mighty's Connector album.
 addToTracks(tracksConnector, 'An Epilogue as a Prologue', '1:42');
 addToTracks(tracksConnector, 'Lady of Death', '3:47');
 addToTracks(tracksConnector, 'The Lying Eyes of Miss Erray', '4:07');
@@ -66,13 +84,15 @@ addToTracks(tracksConnector, '(No) Faith in Fate', '4:18');
 addToTracks(tracksConnector, 'The Frame I: Betrayal in the Watchtower', '5:25');
 console.log(tracksConnector);
 
+//Finally, adding the track and duration for Absofacto's History Books single.
 addToTracks(tracksBooks, 'History Books', '3:58');
 console.log(tracksBooks);
 
 
 
 
-
+//Function to add a new album to the collection array. The function takes the title, artist, year published, and associated tracks array as input.
+//Then the function fills an album object with the provided data, pushes the album into the collection array, and finally returns the album.
 function addToCollection(title, artist, yearPublished, tracks) {
   let album = {
     title: title,
@@ -84,6 +104,7 @@ function addToCollection(title, artist, yearPublished, tracks) {
   return album;
 }
 
+//Adds the corresponding title, artist, year published, and the corresponding tracks array to array using the above addToCollection function. Prints the result to the console log. 
 console.log(addToCollection('The Great War', 'Sabaton', '2019', tracksGreat));
 console.log(addToCollection('Appeal to Reason', 'Rise Against', '2008', tracksReason));
 console.log(addToCollection('Star Eater', 'Daniel Deluxe', '2015', tracksEater));
@@ -100,7 +121,7 @@ function showCollection(array) {
   console.log(array.length);
   for (let i = 0; i < array.length; i++) {
     console.log(collection[i].title + ' by ' + collection[i].artist + ', published in ' + collection[i].yearPublished + ':');
-    //Nested for loop that 
+    //Nested for loop that loops through the array of tracks in each album, printing the track name and its duration to the console log.
     for (let j = 0; j < collection[i].tracks.length; j++) {
       console.log((j + 1) + '. ' + collection[i].tracks[j].name + ': ' + collection[i].tracks[j].duration);
     }
@@ -127,7 +148,8 @@ console.log(findByArtist('Nickelback'));
 
 
 //STRETCH GOALS
-
+//Function to search for a track in the collection by taking the artist, year, and the track name as input.
+//Takes those inputs and fills a searchObject object to then compare those inputs with the collection array.
 function search(artist, year, trackName) {
   let results = [];
   let searchObject = {
@@ -136,14 +158,19 @@ function search(artist, year, trackName) {
     trackName: trackName
   }
 
+  //If the provided search parameters are empty or undefined, then the function will return the whole collection.
   if (searchObject.searchArtist === undefined && searchObject.searchYear === undefined && searchObject.trackName === undefined) {
     return collection;
   }
 
+  //Otherwise, the function will loop through the entire collection array and compare the artist and year parameters in the searchObject object with those present in each album of the collection.
   else {
     for (let i = 0; i < collection.length; i++) {
+      //Compares the searchYear and searchArtist parameters with the corresponding values in the collection array.
       if (searchObject.searchYear === collection[i].yearPublished && searchObject.searchArtist === collection[i].artist) {
+        //If BOTH of those parameters are present in the collection array, it then loops through the tracks array in each matching album, comparing the trackName from the search parameters with the track names in those albums being looped through.
         for (let j = 0; j < collection[i].tracks.length; j++) {
+          //If there is the same track name present in both, then the function pushes the corresponding artist and that track name to the results array.
           if (searchObject.trackName === collection[i].tracks[j].name) {
             results.push(collection[i].artist);
             results.push(collection[i].tracks[j].name);
@@ -152,6 +179,7 @@ function search(artist, year, trackName) {
       }
     }
   }
+  //Returns the results array. Will return an empty array if the searched for object is not present in the collection.
   return results;
 }
 
@@ -164,12 +192,3 @@ console.log(search('Ray Charles', '1957'));
 
 //The search will return the entire collection since the search input was left empty.
 console.log(search());
-
-function addToTracks(tracks, name, duration) {
-  let track = {
-    name: name,
-    duration: duration
-  }
-  tracks.push(track);
-  return track;
-}
